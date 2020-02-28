c168626de0521382d97457dcff8d965f3dd8cb5b
Gustavo Bergantin <gustavo.bindandi@gmail.com>
2020-02-28 15:57:16 -0300

    first commit

---
+++ tests/TestCase/Controller/ProductsControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Stock\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+use Stock\Controller\ProductsController;
+
+/**
+ * Stock\Controller\ProductsController Test Case
+ *
+ * @uses \Stock\Controller\ProductsController
+ */
+class ProductsControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
